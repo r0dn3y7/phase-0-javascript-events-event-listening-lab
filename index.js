2306af29910ff2a29bff74bc35b430cb0aa26986
Rodney <rodney.amani@student.moringaschool.com>
2025-03-19 21:00:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const button = document.getElementById("button"); 
+    
+    if (button) {  
+        button.addEventListener("click", () => {
+            console.log("Button clicked!");
+        });
+    }
 }
+
+addingEventListener(); 
